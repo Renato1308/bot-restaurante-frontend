62a26648b0a4219d458b1ab0e34c1a746888b01f
Renato1308 <renato130887@gmail.com>
2026-07-30 11:06:36 -0300

    Adiciona DJANGO_SETTINGS_MODULE no index.py e ajusta vercel.json

--- api/index.py
+++ api/index.py
@@ -4,6 +4,9 @@ import sys
 # Adiciona o diretório raiz do projeto ao Python Path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+# Define as configurações do Django explicitamente para o ambiente Serverless
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
 from core.wsgi import application
 
 # A Vercel procura por uma variável chamada 'app'
